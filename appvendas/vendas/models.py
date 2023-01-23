51eacc1381534312549bb352fb6a5adca882bff3
Eder Assis <109834675+assis1983@users.noreply.github.com>
2023-01-23 17:02:31 -0300

    Cria o APP de Vendas usando o Django Admin

---
+++ appvendas/vendas/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Cliente(models.Model):
+
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=11, unique=True)
+
+class Pedido(models.Model):
+
+    cliente = models.ForeignKey(Cliente, models.CASCADE)
+    data = models.DateField()
+    observacao = models.TextField(blank=True)
+
+class Item(models.Model):
+    
+    pedido = models.ForeignKey(Pedido, models.CASCADE)
+    produto = models.CharField(max_length=100)
+    quantidade = models.IntegerField(default=1)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
